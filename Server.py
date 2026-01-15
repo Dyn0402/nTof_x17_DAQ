28a93dd176748dbc3b57e4db5f2690c9522b76b8
Dylan Neff <Dyn0402@hotmail.com>
2026-01-16 00:30:14 +0100

    Added cosmic bench daq things

---
+++ Server.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on April 29 8:41 PM 2024
+Created in PyCharm
+Created as Cosmic_Bench_DAQ_Control/Server.py
+
+@author: Dylan Neff, Dylan
+"""
+
+import socket
+import time
+import json
+import struct
+
+
+class Server:
+    def __init__(self, port=1100):
+        self.port = port
+        self.server_host = '0.0.0.0'
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket = None
+        self.client_address = None
+        self.max_recv = 1024 * 1000  # Max bytes to receive
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.client_socket is not None:
+            self.client_socket.close()
+        self.server.close()
+        print('Server closed')
+
+    def start(self):
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow immediate reuse of address
+        self.server.bind((self.server_host, self.port))
+        self.server.listen(1)
+        print(f"Listening on {self.server_host}:{self.port}")
+        self.accept_connection()
+
+    def accept_connection(self):
+        while True:
+            try:
+                self.client_socket, self.client_address = self.server.accept()
+                print(f"{self.client_address[0]}:{self.client_address[1]} Connected")
+                break
+            except socket.error:
+                print("Connection error. Retrying...")
+                time.sleep(1)
+
+    def receive(self):
+        data = self.client_socket.recv(self.max_recv).decode()
+        print(f"Received: {data}")
+        return data
+
+    def receive_json(self):
+        # Read the length header first
+        length_header = self.client_socket.recv(4)
+        if not length_header:
+            return None
+
+        length = struct.unpack('!I', length_header)[0]
+        data = b''
+
+        while len(data) < length:
+            packet = self.client_socket.recv(self.max_recv)
+            data += packet
+
+        data = json.loads(data.decode())
+        print(f"Received: {data}")
+        return data
+
+    def send(self, data, silent=False):
+        self.client_socket.send(data.encode())
+        if not silent:
+            print(f"Sent: {data}")
+
+    def send_json(self, data):
+        json_data = json.dumps(data).encode()
+        length = struct.pack('!I', len(json_data))  # Pack length as a 4-byte unsigned integer
+        self.client_socket.sendall(length + json_data)
+        print(f"Sent: {data}")
+
+    def set_blocking(self, blocking=True):
+        self.client_socket.setblocking(blocking)
+
+    def set_timeout(self, timeout):
+        self.client_socket.settimeout(timeout)
+
+    def get_timeout(self):
+        return self.client_socket.gettimeout()
---
+++ common_functions.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on May 13 4:44 PM 2024
+Created in PyCharm
+Created as Cosmic_Bench_DAQ_Control/common_functions.py
+
+@author: Dylan Neff, Dylan
+"""
+
+import os
+from datetime import datetime
+import time
+
+
+def create_dir_if_not_exist(dir_path):
+    if not os.path.isdir(dir_path):
+        os.makedirs(dir_path)
+        os.chmod(dir_path, 0o777)
+
+
+def get_date_from_fdf_file_name(file_name):
+    """
+    Get date from file name with format ...xxx_xxx_240212_11H42_000_01.xxx
+    :param file_name:
+    :return:
+    """
+    date_str = file_name.split('_')[-4] + ' ' + file_name.split('_')[-3]
+    date = datetime.strptime(date_str, '%y%m%d %HH%M')
+    return date
+
+
+def get_feu_num_from_fdf_file_name(file_name):
+    """
+    Get fdf style feu number from file name with format ...xxx_xxx_240212_11H42_000_01.xxx
+    :param file_name:
+    :return:
+    """
+    fdf_num = int(file_name.split('_')[-1].split('.')[0])
+    return fdf_num
+
+
+def get_file_num_from_fdf_file_name(file_name, num_index=-2):
+    """
+    Get fdf style file number from file name with format ...xxx_xxx_240212_11H42_000_01.xxx
+    Updated to more robustly get first number from back.
+    :param file_name:
+    :param num_index:
+    :return:
+    """
+    file_split = remove_after_last_dot(file_name).split('_')
+    file_nums = []
+    for x in file_split:
+        try:
+            file_nums.append(int(x))
+        except ValueError:
+            pass
+    return file_nums[num_index]
+
+
+def remove_after_last_dot(input_string):
+    # Find the index of the last dot
+    last_dot_index = input_string.rfind('.')
+
+    # If there's no dot, return the original string
+    if last_dot_index == -1:
+        return input_string
+
+    # Return the substring up to the last dot (not including the dot)
+    return input_string[:last_dot_index]
+
+
+def get_run_name_from_fdf_file_name(file_name):
+    file_name_split = file_name.split('_')
+    run_name_end_index = 0
+    for i, part in enumerate(file_name_split):  # Find xxHxx in file name split
+        if len(part) == 5 and part[2] == 'H' and is_convertible_to_int(part[:2]) and is_convertible_to_int(part[3:]):
+            run_name_end_index = i
+            break
+    run_name = '_'.join(file_name_split[:run_name_end_index + 1])
+    return run_name
+
+
+def is_convertible_to_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def wait_for_copy_complete(filepath, check_interval=1.0, stable_time=3.0, wait_for_creation=False):
+    """
+    Wait until file size stops changing for 'stable_time' seconds.
+
+    Args:
+        filepath (str): Path to file to check.
+        check_interval (float): Seconds between size checks.
+        stable_time (float): Time file size must remain constant to be considered complete.
+        wait_for_creation (bool):
+            - If True, keep waiting until the file appears.
+            - If False, return False immediately if file does not exist.
+
+    Returns:
+        bool: True if file appears and stabilizes, False otherwise.
+    """
+    last_size = -1
+    stable_start = None
+
+    while True:
+        if not os.path.exists(filepath):
+            if not wait_for_creation:
+                return False
+            stable_start = None
+            time.sleep(check_interval)
+            continue
+
+        current_size = os.path.getsize(filepath)
+
+        if current_size == last_size:
+            if stable_start is None:
+                stable_start = time.time()
+            elif time.time() - stable_start >= stable_time:
+                return True  # File size stable long enough
+        else:
+            stable_start = None
+            last_size = current_size
+
+        time.sleep(check_interval)
