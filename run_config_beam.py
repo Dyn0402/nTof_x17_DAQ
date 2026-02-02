86b718fa18e94bd6a1132a2a2409b11bf03bd21e
Dylan Neff <Dyn0402@hotmail.com>
2026-02-02 09:52:38 +0100

    run_config.py more update pedestals path

--- run_config_beam.py
+++ run_config_beam.py
@@ -64,7 +64,7 @@ class Config(RunConfigBase):
             'copy_on_fly': True,  # True to copy raw data to out dir during run, False to copy after run
             'batch_mode': True,  # Run Dream RunCtrl in batch mode. Not implemented for cosmic bench CPU.
             'zero_suppress': False,  # True to run in zero suppression mode, False to run in full readout mode
-            'pedestals_dir': f'{self.base_out_dir}pedestal/',  # None to ignore, else top directory for pedestal runs
+            'pedestals_dir': f'{self.base_out_dir}pedestals/',  # None to ignore, else top directory for pedestal runs
             'pedestals': 'latest',  # 'latest' for most recent, otherwise specify directory name, eg "pedestals_10-22-25_13-43-34"
             'latency': 33,  # Latency setting for DAQ in clock cycles
             'sample_period': 40,  # ns, sampling period
