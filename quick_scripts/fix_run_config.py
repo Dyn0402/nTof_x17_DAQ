edba5f37d1ef13366c5ff2558ab7ad4bbb0116b8
Dylan Neff <Dyn0402@hotmail.com>
2026-02-05 11:39:35 +0100

    dream_daq_control.py hopefully pedestal_run.txt works, run_config_beam.py set for lead HV scan

---
+++ quick_scripts/fix_run_config.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on February 04 11:11 PM 2026
+Created in PyCharm
+Created as nTof_x17_DAQ/fix_run_config.py
+
+@author: Dylan Neff, dylan
+"""
+
+import json
+
+
+def main():
+    base_path = '/media/dylan/data/x17/feb_beam/runs/'
+    run = 'run_25'
+    sub_run = 'resist_450V_drift_600V'
+
+    run_config_path = f'{base_path}{run}/run_config.json'
+
+    with open(run_config_path) as f:
+        run_cfg = json.load(f)
+    print(run_cfg)
+
+    run_cfg = fix_dream_feus(run_cfg)
+
+    with open(run_config_path, 'w') as f:
+        json.dump(run_cfg, f)
+
+    print('donzo')
+
+
+def fix_dream_feus(run_cfg):
+    """
+    Fix DREAM feus
+    Args:
+        run_cfg:
+
+    Returns:
+
+    """
+    det_name = 'mx17_1'
+
+    for det in run_cfg['detectors']:
+        print(det['name'])
+        if det['name'] != det_name:
+            continue
+        print(det['dream_feus'])
+        for connector in det['dream_feus']:
+            if connector.startswith('y_'):
+                det['dream_feus'][connector][0] = 5
+
+    print(run_cfg['detectors'][0]['dream_feus'])
+
+    return run_cfg
+
+
+if __name__ == '__main__':
+    main()
